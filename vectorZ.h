7d1fb7fa1cb423de2125817ed39197aa09cba4e8
Vadim Sokolov <varem99@mail.ru>
2019-03-11 21:57:34 +0300

    review
    error check, operators, some answers

--- vectorZ.h
+++ vectorZ.h
@@ -6,6 +6,17 @@
 #define VECTORZUI_VECTORZ_H
 #include <iostream>
 
+// use brackets with params in macro
+
+
+// why do you write like
+error_ = ERROR;
+// ????
+
+// if you have your ERORRS enum using binary shifts?
+// then why do you use binary shifts?
+// think about It, bro
+
 #define CHECK_ERR(CONDITION, ERROR)  \
     do{                              \
         if(CONDITION)                \
@@ -30,12 +41,16 @@ enum ERRORS
     NEGATIVE_CAPACITY   = 1 << 5,
     VECTOR_WRONG_HASH   = 1 << 6
 };
+
+// interesting Constants
+
 enum BIRDS
 {
     CANARY_ONE = 282,
     CANARY_TWO = 202
 };
 typedef int vctrElemT;
+
 class Vector
 {
     public:
@@ -44,29 +59,41 @@ class Vector
         Vector(const Vector&);
         ~Vector();
 
+    
+    //----------------------------------------------------------------
+    
         int push(vctrElemT);
         int pop();
         int peek();
 
+    //----------------------------------------------------------------
+    
         vctrElemT& operator [] (size_t);
         Vector& operator = (Vector&);
+        // too few operators
+        // you have to override ~8 operators
 
     private:
         size_t elemNum_;
         size_t capacity_;
 
 
+    //----------------------------------------------------------------
         int canary1_;
         vctrElemT* data_;
         int canary2_;
+    
+    //----------------------------------------------------------------
 
         int error_;
         unsigned int hash_;
-        unsigned int Hash();
-        int VectorOk();
-        int resize();
-        void swap(Vector&);
-        int dump();
+    
+    //----------------------------------------------------------------
+        unsigned int    Hash();
+        int             VectorOk();
+        int             resize();
+        void            swap(Vector&);
+        int             dump();
 
 };
 #endif //VECTORZUI_VECTORZ_H
